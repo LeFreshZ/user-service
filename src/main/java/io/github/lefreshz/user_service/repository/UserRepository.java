256cde8427ef010faf46aa94ed2f827eae67e722
lefreshz <scver1@yandex.ru>
2026-05-15 19:05:33 +0300

    Add repository annotations for clarity

--- src/main/java/io/github/lefreshz/user_service/repository/UserRepository.java
+++ src/main/java/io/github/lefreshz/user_service/repository/UserRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
   Optional<User> findByEmail(String email);
