6544ff1bfd54135c9df014010494c5b61b659939
lefreshz <scver1@yandex.ru>
2026-05-18 13:47:19 +0300

    Add cascade deleting for cards

--- src/main/java/io/github/lefreshz/user_service/entity/User.java
+++ src/main/java/io/github/lefreshz/user_service/entity/User.java
@@ -1,5 +1,6 @@
 package io.github.lefreshz.user_service.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -39,6 +40,10 @@ public class User extends BaseEntity {
   @Column(name = "is_active", nullable = false)
   private Boolean active;
 
-  @OneToMany(mappedBy = "user")
+  @OneToMany(
+      mappedBy = "user",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true
+  )
   private List<PaymentCard> paymentCards;
 }
